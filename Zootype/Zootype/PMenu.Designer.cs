62243b7e4b78da0f61357cba7449676ad1bae82d
VivirfHerrera <58118055+VivirfHerrera@users.noreply.github.com>
2020-09-27 02:53:37 -0700

    Pantalla menu

--- Zootype/Zootype/PMenu.Designer.cs
+++ Zootype/Zootype/PMenu.Designer.cs
@@ -30,24 +30,100 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PMenu));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnInstrucciones = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -9);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(960, 540);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // btnConfig
+            // 
+            this.btnConfig.FlatAppearance.BorderSize = 0;
+            this.btnConfig.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfig.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfig.Image = ((System.Drawing.Image)(resources.GetObject("btnConfig.Image")));
+            this.btnConfig.Location = new System.Drawing.Point(32, 388);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(140, 42);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.Text = "Configuracion";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.FlatAppearance.BorderSize = 0;
+            this.btnRecord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecord.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecord.Image = ((System.Drawing.Image)(resources.GetObject("btnRecord.Image")));
+            this.btnRecord.Location = new System.Drawing.Point(774, 452);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(140, 42);
+            this.btnRecord.TabIndex = 2;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.Image")));
+            this.btnInicio.Location = new System.Drawing.Point(774, 326);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(140, 42);
+            this.btnInicio.TabIndex = 3;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.FlatAppearance.BorderSize = 0;
+            this.btnSalida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalida.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalida.Image = ((System.Drawing.Image)(resources.GetObject("btnSalida.Image")));
+            this.btnSalida.Location = new System.Drawing.Point(32, 462);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(140, 41);
+            this.btnSalida.TabIndex = 4;
+            this.btnSalida.Text = "Salida";
+            this.btnSalida.UseVisualStyleBackColor = true;
+            // 
+            // btnInstrucciones
+            // 
+            this.btnInstrucciones.FlatAppearance.BorderSize = 0;
+            this.btnInstrucciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInstrucciones.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstrucciones.Image = ((System.Drawing.Image)(resources.GetObject("btnInstrucciones.Image")));
+            this.btnInstrucciones.Location = new System.Drawing.Point(774, 388);
+            this.btnInstrucciones.Name = "btnInstrucciones";
+            this.btnInstrucciones.Size = new System.Drawing.Size(140, 42);
+            this.btnInstrucciones.TabIndex = 5;
+            this.btnInstrucciones.Text = "Instrucciones";
+            this.btnInstrucciones.UseVisualStyleBackColor = true;
+            // 
             // PMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 543);
+            this.Controls.Add(this.btnInstrucciones);
+            this.Controls.Add(this.btnSalida);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.btnConfig);
             this.Controls.Add(this.pictureBox1);
             this.Name = "PMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -61,5 +137,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Button btnInstrucciones;
     }
 }
