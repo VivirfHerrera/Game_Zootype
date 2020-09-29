6610d9dd8d376b3908dde97a0cfd771eb9dcf726
VivirfHerrera <58118055+VivirfHerrera@users.noreply.github.com>
2020-09-29 12:53:47 -0700

    Pantalla de Inicio Agregada

--- Zootype/Zootype/PInicio.Designer.cs
+++ Zootype/Zootype/PInicio.Designer.cs
@@ -29,44 +29,44 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PInicio));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnlvl3 = new System.Windows.Forms.Button();
+            this.btnlvl2 = new System.Windows.Forms.Button();
             this.btnlvl1 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnregresarPin = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnlvl3
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkGreen;
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.button1.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.button1.Location = new System.Drawing.Point(28, 382);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Avanzado";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnlvl3.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnlvl3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnlvl3.BackgroundImage")));
+            this.btnlvl3.FlatAppearance.BorderSize = 0;
+            this.btnlvl3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlvl3.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnlvl3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnlvl3.Location = new System.Drawing.Point(28, 382);
+            this.btnlvl3.Name = "btnlvl3";
+            this.btnlvl3.Size = new System.Drawing.Size(150, 44);
+            this.btnlvl3.TabIndex = 0;
+            this.btnlvl3.Text = "Avanzado";
+            this.btnlvl3.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnlvl2
             // 
-            this.button2.BackColor = System.Drawing.Color.DarkGreen;
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.button2.Location = new System.Drawing.Point(28, 308);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Intermedio";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnlvl2.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnlvl2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnlvl2.BackgroundImage")));
+            this.btnlvl2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnlvl2.FlatAppearance.BorderSize = 0;
+            this.btnlvl2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlvl2.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlvl2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnlvl2.Location = new System.Drawing.Point(28, 308);
+            this.btnlvl2.Name = "btnlvl2";
+            this.btnlvl2.Size = new System.Drawing.Size(150, 46);
+            this.btnlvl2.TabIndex = 1;
+            this.btnlvl2.Text = "Intermedio";
+            this.btnlvl2.UseVisualStyleBackColor = false;
             // 
             // btnlvl1
             // 
@@ -83,20 +83,20 @@
             this.btnlvl1.Text = "Principiante";
             this.btnlvl1.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // btnregresarPin
             // 
-            this.button5.BackColor = System.Drawing.Color.DarkGreen;
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.button5.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.button5.Location = new System.Drawing.Point(638, 382);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 44);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Regresar";
-            this.button5.UseVisualStyleBackColor = false;
+            this.btnregresarPin.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnregresarPin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnregresarPin.BackgroundImage")));
+            this.btnregresarPin.FlatAppearance.BorderSize = 0;
+            this.btnregresarPin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnregresarPin.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnregresarPin.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnregresarPin.Location = new System.Drawing.Point(638, 382);
+            this.btnregresarPin.Name = "btnregresarPin";
+            this.btnregresarPin.Size = new System.Drawing.Size(150, 44);
+            this.btnregresarPin.TabIndex = 4;
+            this.btnregresarPin.Text = "Regresar";
+            this.btnregresarPin.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
@@ -114,10 +114,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnregresarPin);
             this.Controls.Add(this.btnlvl1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnlvl2);
+            this.Controls.Add(this.btnlvl3);
             this.Controls.Add(this.pictureBox1);
             this.Name = "PInicio";
             this.Text = "Inicio";
@@ -128,10 +128,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnlvl3;
+        private System.Windows.Forms.Button btnlvl2;
         private System.Windows.Forms.Button btnlvl1;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnregresarPin;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
